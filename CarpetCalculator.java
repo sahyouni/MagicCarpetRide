74a23135d5601747782bb47ab147a4c821c8c8bb
Rami Sahyouni <rami.sahyouni@trulioo.com>
2020-02-22 18:33:21 -0800

    adding my thoughts

--- CarpetCalculator.java
+++ CarpetCalculator.java
@@ -1,85 +1,96 @@
 
 /**
+ * 
+ * we need to brainstorm about this. Why is this class called CarpetCalculator? It seems to just calculate areas. Nothing specific to carpets I feel.
+ * 
  * Class CarpetCalculator to provide info to calculate carpet cost
  *
  * @author (cherry cui)
  * @version (2020-02-22)
  */
-public class CarpetCalculator
-{
-    // instance variables 
+public class CarpetCalculator {
+    // instance variables
     private double pricePerSquareFoot;
     private Room room;
 
     /**
      * Parameterized Constructor for objects of class CarpetCalculator
+     * 
+     * Rami: Do we really want to input a "Room" here or is it just a "rectangle"?
      */
-    public CarpetCalculator(double inputPricePerSquareFoot, Room inputRoom)
-    {
+    public CarpetCalculator(double inputPricePerSquareFoot, Room inputRoom) {
         setPricePerSquareFoot(inputPricePerSquareFoot);
-        setRoom (inputRoom);
+        setRoom(inputRoom);
     }
 
     /**
      * Accessor getPricePerSquareFoot
+     * 
      * @return pricePerSquareFoot
      */
-    public double getPricePerSquareFoot(){
+    public double getPricePerSquareFoot() {
         return pricePerSquareFoot;
     }
-    
+
     /**
      * Accessor getRoom
+     * 
      * @return room
      */
-    public Room getRoom(){
+    public Room getRoom() {
         return room;
     }
-    
+
     /**
      * Mutator setPricePerSquareFoot
-     * @param inputPricePerSquareFoot A parameter to set value of price per square foot
+     * 
+     * @param inputPricePerSquareFoot A parameter to set value of price per square
+     *                                foot
      */
-  
-    public void setPricePerSquareFoot(double inputPricePerSquareFoot){
-        if (inputPricePerSquareFoot > 0){
-        pricePerSquareFoot = inputPricePerSquareFoot;
-        } else {
-        pricePerSquareFoot = 0.0;
+
+    public void setPricePerSquareFoot(double inputPricePerSquareFoot) {
+
+        if (inputPricePerSquareFoot <= 0) {
+            throw new IllegalArgumentException();
         }
+
+        pricePerSquareFoot = inputPricePerSquareFoot;
+
+        // dont play these games of accepting wrong input
+        /*
+         * if (inputPricePerSquareFoot > 0) { pricePerSquareFoot =
+         * inputPricePerSquareFoot; } else { pricePerSquareFoot = 0.0; }
+         */
     }
-    
+
     /**
      * Mutator setRoom
+     * 
      * @param inputRoom A parameter to set value of room
      */
-  
-    public void setRoom(Room inputRoom){
-        if (inputRoom != null){
+
+    public void setRoom(Room inputRoom) {
         room = inputRoom;
-        } else {
-        room = new Room();
-        }
     }
+
     /**
      * Method calculateTotalCost
+     * 
      * @return the total cost of the carpet for that room
      */
-    public double calculateTotalCost()
-    {
+    public double calculateTotalCost() {
         double totalCost = pricePerSquareFoot * room.calculateArea();
-        return totalCost ;
+        return totalCost;
     }
-    
+
     /**
      * Method displayDetails
      */
-    public void displayDetails()
-    {
-        System.out.println ("Length of the room in ft: " + room.getLengthInFeet());
-        System.out.println ("Width of the room in ft: " + room.getWidthInFeet());
-        System.out.println ("Area of the room in sqft: " + room.calculateArea());
-        System.out.println ("Price per square foot: " + "$" + pricePerSquareFoot);
-        System.out.println ("Total cost of the carpet: " + "$" + calculateTotalCost());
+    public void displayDetails() {
+        System.out.println("Length of the room in ft: " + room.getLengthInFeet());
+        System.out.println("Width of the room in ft: " + room.getWidthInFeet());
+        System.out.println("Area of the room in sqft: " + room.calculateArea());
+        System.out.println("Price per square foot: " + "$" + pricePerSquareFoot);
+        System.out.println("Total cost of the carpet: " + "$" + calculateTotalCost());
     }
 }
--- Room.java
+++ Room.java
@@ -1,12 +1,12 @@
 
 /**
  * Class Room to provide info to calculate the room area.
+ * Rami: I don't see any "room" specific functionality here. This class could be called a Rectangle instead?
  *
  * @author (Cherry Cui)
  * @version (2020-02-22)
  */
-public class Room
-{
+public class Room {
     // instance variables
     private double lengthInFeet;
     private double widthInFeet;
@@ -14,68 +14,67 @@ public class Room
     /**
      * No args constructor for objects of class Room
      */
-    public Room()
-    {
+    public Room() {
         // initialise instance variables to the default value
         lengthInFeet = 0.0;
         widthInFeet = 0.0;
     }
-    
+
     /**
      * Parameterized constructor for objects of class Room
      */
-    public Room(double inputLengthInFeet, double inputWidthInFeet)
-    {
+    public Room(double inputLengthInFeet, double inputWidthInFeet) {
         setLengthInFeet(inputLengthInFeet);
         setWidthInFeet(inputWidthInFeet);
-      
+
     }
 
     /**
-     * Accessor getLengthInFeet
-     * return lengthInFeet
+     * Accessor getLengthInFeet return lengthInFeet
      */
-    public double getLengthInFeet(){
+    public double getLengthInFeet() {
         return lengthInFeet;
     }
-    
+
     /**
-     * Accessor getWidththInFeet
-     * return widthInFeet
+     * Accessor getWidththInFeet return widthInFeet
      */
-    public double getWidthInFeet(){
+    public double getWidthInFeet() {
         return widthInFeet;
     }
-    
+
     /**
      * Mutator setLengthInFeet
+     * 
      * @param inputLengthInFeet A parameter to set value of length in feet
      */
-    public void setLengthInFeet(double inputLengthInFeet){
-        if(inputLengthInFeet > 0){
+    public void setLengthInFeet(double inputLengthInFeet) {
+
+        if (inputLengthInFeet <= 0) {
+            throw new IllegalArgumentException("Length must be positive number");
+        }
+
         lengthInFeet = inputLengthInFeet;
-        }else {
-        throw new IllegalArgumentException ("Length must be positive number");
-        };
     }
-    
+
     /**
      * Mutator setWidthInFeet
+     * 
      * @param inputWidthInFeet A parameter to set value of width in feet
      */
-    public void setWidthInFeet(double inputWidthInFeet){
-        if(inputWidthInFeet > 0){
+    public void setWidthInFeet(double inputWidthInFeet) {
+        if (inputWidthInFeet <= 0) {
+            throw new IllegalArgumentException("Width must be positive number");
+        }
         widthInFeet = inputWidthInFeet;
-        }else {
-        throw new IllegalArgumentException ("Width must be positive number");
-        };
     }
+
     /**
      * Method calculateArea
+     * 
      * @return the room area in square feet
      */
-    public double calculateArea()
-    {
+    public double calculateArea() {
         double area = lengthInFeet * widthInFeet;
         return area;
     }
